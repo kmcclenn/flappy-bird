825f8a32b3c3a2453f8c91c16daca6b0792a4d2b
Kai McClennen <kmcclenn@mit.edu>
2024-07-20 01:11:03 -0400

    flappy bird simulator with pygame

---
+++ constants.py
@@ -0,0 +1,11 @@
+class Constants:
+    GRAVITY = 12 # downward acceleration
+    JUMP = -7 # jump velocity
+    SPACE_RESET = 100 # ms between jumps
+    BACKGROUND = (5, 175, 242) # the background color
+    PIPE_WIDTH = 40 # width of pipe in pixels
+    PIPE_SPEED = 200 # speed of pipe in pixels per second
+    PIPE_ACCELERATION = 0.1 # acceleration of pipes
+    PIPES_PER_TIME = 2 # number of pipes to show at a time
+    PIPE_GAP = 150 # gap in pipe in pixels
+    PLAYER_SIZE = 10 # radius of player in pixels
